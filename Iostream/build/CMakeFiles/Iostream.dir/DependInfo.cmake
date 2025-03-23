8cf0da7facd68642815ba2d86e143595702d9207
YHL <1652966670@qq.com>
2025-03-23 19:22:50 +0800

    update Iostream

--- Iostream/build/CMakeFiles/Iostream.dir/DependInfo.cmake
+++ Iostream/build/CMakeFiles/Iostream.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/vsproject/AAA/Iostream/main.cpp" "CMakeFiles/Iostream.dir/main.cpp.obj" "gcc" "CMakeFiles/Iostream.dir/main.cpp.obj.d"
+  "D:/vsproject/AAA/C-demo/Iostream/main.cpp" "CMakeFiles/Iostream.dir/main.cpp.obj" "gcc" "CMakeFiles/Iostream.dir/main.cpp.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- Iostream/main.cpp
+++ Iostream/main.cpp
@@ -25,20 +25,34 @@
 using namespace std;
 
 int main() {
+    // //全缓冲
+    // //创建文件test.txt并打开
+    // ofstream outfile("test.txt");
+    // //向test.txt文件中写入4096个字符'a'，缓冲区极限是4096，n为4095为分界线
+    // for(int n=0;n<4095;n++)
+    // {
+    //     outfile << 'a';
 
-    //创建文件test.txt并打开
-    ofstream outfile("test.txt");
-    //向test.txt文件中写入4096个字符’a’
-    for(int n=0;n< 20;n++)
-    {
-        outfile << 'a';
-    }
-    //暂停，按任意键继续
-    system("PAUSE");
-    //继续向test.txt文件中写入字符’b’，也就是说，第4097个字符是’b’
-    outfile << 'b';
-    //暂停，按任意键继续
-    system("PAUSE");
+    //     //outfile<<std::flush;
+    // }
+    // //暂停，按任意键继续
+    // system("PAUSE");
+    // //继续向test.txt文件中写入字符’b’，也就是说，第4097个字符是’b’
+    // outfile << 'b';
+    // //暂停，按任意键继续
+    // system("PAUSE");
 
+
+    // //行缓冲
+
+    // char c;
+
+    // c = getchar();//输入缓冲区
+    // cout<<c<<endl;//清空输出缓冲区
+    // system("PAUSE");
+    // while(((c=getchar())!='\n')){
+    //     printf("%c", c);
+    // }
+    // system("PAUSE");
 
 }
